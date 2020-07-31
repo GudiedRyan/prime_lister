4e9ab6e6f9e85852d7cdc1f3ef65ba30fd78734a
Ryan Guide <ryan.guide@comcast.net>
2020-07-31 17:52:58 -0400

    Updates

---
+++ precache-manifest.ab6d4b7c2f46a945f62495ce6235efab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e543e903d6a9d5468be33ebaa927e927",
+    "url": "/prime_lister/index.html"
+  },
+  {
+    "revision": "62cd5be5b1752a26e792",
+    "url": "/prime_lister/static/css/main.3b9d1fb6.chunk.css"
+  },
+  {
+    "revision": "5c0d88230873e594f187",
+    "url": "/prime_lister/static/js/2.aeb94c14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prime_lister/static/js/2.aeb94c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62cd5be5b1752a26e792",
+    "url": "/prime_lister/static/js/main.5670c8d0.chunk.js"
+  },
+  {
+    "revision": "033b22bd71e452089a5c",
+    "url": "/prime_lister/static/js/runtime-main.b315da1a.js"
+  }
+]);
